70563c74a05f4f591d721f2512ea08643e14923e
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-12-24 07:29:41 +0530

    Call downloadAndTweet every 40 min

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
-const fetch = require('node-fetch')
-const Twitter = require('twitter')
-const { config } = require('./config')
-const client = new Twitter(config)
-
-fetch('http://www.reddit.com/r/todayilearnt/new.json?sort=hot')
-.then(v => v.json())
-.then(v => reduceToObject(v.data.children))
-.then(arr => arr.forEach(v => {
-  let status = { status: v.title + ' ' + v.url }
-  client.post('statuses/update', status)
-  .then(tweet => tweet)
-  .catch(err => {
-    console.log(err[0].message)
-  })
-}))
-.catch(err => {
-  console.log(err[0].message)
-})
-
-const reduceToObject = arr => {
-  return arr.reduce((a, v) => {
-    let data = v.data
-    let obj = {}
-    obj.id = data.subreddit_id
-    obj.title = data.title
-    obj.url = data.url
-    a.push(obj)
-    return a
-  }, [])
-}
+const {downloadAndTweet} = require('./lib/app.js')
+console.log('Running now...')
+downloadAndTweet()
+setInterval(() => downloadAndTweet(), 14 * 60 * 60)
